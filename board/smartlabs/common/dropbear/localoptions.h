906607aa8c19402929254111b287b076a65b1bb8
Alexander Shmelev <ashmelev@smartlabs.tv>
2020-04-08 00:48:11 +0300

    IMPR: initial support of smartlabs/sml_hi3798mv200

---
+++ board/smartlabs/common/dropbear/localoptions.h
@@ -0,0 +1,10 @@
+#if !HAVE_CRYPT
+#define DROPBEAR_SVR_PASSWORD_AUTH 0
+#endif
+#define DROPBEAR_3DES 0
+#define DROPBEAR_ENABLE_CBC_MODE 0
+#define DROPBEAR_SHA1_96_HMAC 0
+#define DROPBEAR_DSS 0
+#define DROPBEAR_DH_GROUP1 0
+#define DO_HOST_LOOKUP 1
+#define DEFAULT_PATH "/bin:/sbin:/usr/bin:/usr/sbin"
---
+++ board/smartlabs/sml_hi3798mv200/post_build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mkdir -p "${TARGET_DIR}/flash"
