93a408a8d0086dc16b80ec1bffe42e87da967e7e
Alexander Shmelev <ashmelev@smartlabs.tv>
2020-04-10 13:19:04 +0300

    sml_hi3798mv200: add basic STB support

--- board/smartlabs/sml_hi3798mv200/post_build.sh
+++ board/smartlabs/sml_hi3798mv200/post_build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 mkdir -p "${TARGET_DIR}/flash"
+mkdir -p "${TARGET_DIR}/mnt/persistent"
+
+# u-boot environment
+ln -s /tmp/fw_env.conf ${TARGET_DIR}/etc/fw_env.conf
+
+# Change PYTHONHOME to rw directory
+echo "" >> ${TARGET_DIR}/etc/profile
+echo "export PYTHONHOME=/mnt/persistent/usr" >> ${TARGET_DIR}/etc/profile
+
+# Add python scrypts to path
+echo "export PATH=/mnt/persistent/usr/bin:\$PATH" >> ${TARGET_DIR}/etc/profile
